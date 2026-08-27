66ac5b5de4b8f9522fa2aadc4441648d48db0e21
Arkin Kukadia <arkin.kukadia27@pallottihs.info>
2026-08-27 11:57:25 -0400

    completed 1.2 activity

--- lesson 1.2/ParkingMeter.java
+++ lesson 1.2/ParkingMeter.java
@@ -3,17 +3,18 @@ public class ParkingMeter
     public static void main(String[] args) 
     {
         // Step 1: Create a variable to represent how much money has been put into this meter.
-
+        double meter;
         // Step 2: Change the value of your variable to store $2.50.
-
+        meter = 2.5;
         // Step 3: Create a new variable to represent whether the meter is in use.
-
+        boolean isInUse;
         // Step 4: Set the meter to be in use.
-
+        isInUse = true;
         // Step 5: Set the balance back to $0.
-
+        meter = 0;
         // Step 6: Create a variable to represent how many people have used this meter.
-
+        int people;
         // Step 7: Attempt to set the value of this to 7.5. What happens?
+        //Won't compile because you can't have .5 people
     }    
 }
