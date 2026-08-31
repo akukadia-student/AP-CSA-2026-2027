6abef2c6b63a21850979bbb7899f55dd908dc41f
matt-merritt-phs <matthewmerritt@pallottihs.org>
2026-08-31 11:37:33 -0400

    added guided activities for 1.3

---
+++ lesson 1.3/Errors.java
@@ -0,0 +1,13 @@
+public class Errors {
+    public static void main(String[] args) {
+        // Part 2 - Errors
+
+        // Step 1: Create a double variable, and store the value 5 in it.
+
+        // Step 2: Print the value. Did anything happen?
+
+        // Step 3: Create an integer variable, and store the value 3.2 in it.
+
+        // Step 4: Print the value. Did anything happen?
+    }
+}
