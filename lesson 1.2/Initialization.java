f6a51789de07db5c3c04170c2295cc3ed1ce774b
matt-merritt-phs <matthewmerritt@pallottihs.org>
2026-09-01 12:00:34 -0400

    added demonstration of how to separately declare and initialize variables

---
+++ lesson 1.2/Initialization.java
@@ -0,0 +1,12 @@
+public class Initialization {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        int z = 0;
+
+        double decimal; // declaration (tells java what type decimal is)
+        decimal = 1; // initialization (tells java that we have a value and are ready to use it)
+
+        System.err.println(z);
+    }
+}
