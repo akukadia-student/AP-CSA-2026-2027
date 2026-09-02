74999620f3a85e317e56814868462f1de6a4781d
Arkin Kukadia <arkin.kukadia27@pallottihs.info>
2026-09-02 12:04:40 -0400

    1.3

--- lesson 1.3/Errors.java
+++ lesson 1.3/Errors.java
@@ -3,11 +3,14 @@ public class Errors {
         // Part 2 - Errors
 
         // Step 1: Create a double variable, and store the value 5 in it.
-
+        double number = 5;
         // Step 2: Print the value. Did anything happen?
-
+        System.out.println(number);
+        //It turned from an int to a double
         // Step 3: Create an integer variable, and store the value 3.2 in it.
-
+        int value = (int) 3.2;
+        System.out.println(value);
         // Step 4: Print the value. Did anything happen?
+        // You can't convert a double to an int
     }
 }
--- lesson 1.3/Expressions.java
+++ lesson 1.3/Expressions.java
@@ -3,20 +3,26 @@ public class Expressions {
         // Part 1 - Integers
 
         // Step 1: Create three integer variables with values.
-
+        int one = 3;
+        int two = 2;
+        int three = 3;
         // Step 2: Create a sum variable to store the sum of all three.
-
+        int sum = one + two + three;
         // Step 3: Create an average integer variable to store the mean value.
-
+        int mean = sum / 3;
         // Step 4: Print the result of your average. Is it correct?
-
+        System.out.println(mean);
+        //No it is cut off the decimal
         // Step 5: Create a second average variable to properly compute the average.
-
-
+        double mean2 = sum / 3.0;
+        System.out.println(mean2);
         // Part 2 - Questions
 
         // Question 1: What is the data type for the sum of two integers?
+        //int
         // Question 2: What is the data type for the quotient of two integers?
+        //int
         // Question 3: What is the data type for the product of an integer and a double?
+        //double
     }
 }
