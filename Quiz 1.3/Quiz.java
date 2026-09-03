02a49b2be8e39bfa885a48e05bee1646b5ecd93e
Arkin Kukadia <arkin.kukadia27@pallottihs.info>
2026-09-03 12:05:33 -0400

    quiz

---
+++ Quiz 1.3/Quiz.java
@@ -0,0 +1,13 @@
+public class Quiz {
+    public static void main(String[] args) {
+        int counter = 39;
+        counter = 53;
+        int total = counter + 25;
+        double earned = 10.99;
+        System.out.print("Earnings: ");
+        System.out.println(counter * earned);
+        double average = counter/2.0;
+        System.out.print("Attendees per day: ");
+        System.out.print(average);
+    }
+}
