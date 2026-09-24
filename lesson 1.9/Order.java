028a4e64af4be6f78d611a1371ea4bf0883d609b
matt-merritt-phs <matthewmerritt@pallottihs.org>
2026-09-24 11:01:52 -0400

    added blank exercise for 1.9

---
+++ lesson 1.9/Order.java
@@ -0,0 +1,20 @@
+public class Order
+{
+    public static void main(String[] args) 
+    {
+        // Step 2: Call your scoring method with three different scores.
+        //  One homework assignment with a total 20 points and a score of 18.
+        //  One quiz with a total of 15 points and a score of 14.
+        //  One test with a total of 30 points and a score 29.
+
+        // Step 4: Call your weighting method with the scores from the three assignments.
+    }
+
+    // Step 1: Write a method to calculate the percentage score on an assessment.
+    //  You will need to take inputs for the points earned and total points.
+    //  Your method should return the score as a percentage, and it should support decimals.
+
+    // Step 3: Write a method to calculate the weighted average of the homework, quiz, and test score.
+    //  The homework is 20%, quiz is 30%, and test is 50%.
+    //  Your method should return the score as a percentage, and it should round to the nearest integer. 
+}
